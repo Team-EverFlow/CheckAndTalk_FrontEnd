fc4c0b1d5937a3f23c4c02668cb1b55ed16699d2
Mule129 <a24cc410s@naver.com>
2023-05-29 22:40:40 +0900

    [Feat] Fix Design and Code Style

--- check-and-talk/src/components/Profile.js
+++ check-and-talk/src/components/Profile.js
@@ -1,46 +1,37 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './Profile.css'
 import Profile_red from '../assets/Profile_Red.svg';
-import edit from '../assets/edit_ProfileInfo.svg';
+import edit from '../assets/edit_Profile.svg';
 
-class Profile extends Component {
-    render() {
-        return (
-            <div className='Profile-style'>
-                
+function Profile() {
+    
+    return (
+        <div className='Profile-style'>
+            <div className='Profile-img-container'>
                 <div className='Profile-img'>
                     <img src={Profile_red} width={269}></img>
-                </div>
-                <div className='info-box'>
-                    <div className='Profile-name back-setting'>
-                        <div className='text-box'>Mule</div>
-                        <div className='edit'>
-                            <img src={edit} width={30}></img>
-                        </div>                        
-                    </div>
-                    <div className='Profile-info back-setting'>
-                        <div className='text-box'>상메 글자수 제한해서 크기는 고정시키고 상메 글자수 제한해서 크기는 고정시키고 상메 글자수 제한해서 크기는 고정시키고 상메 글자수 제한해서 크기는 고정시키고 </div>
-                        <div className='edit'>
-                            <img src={edit}></img>
-                        </div>
-                    </div>
-                    <div className='Profile-level'>
-                        <div className='level-stat'>Lv. 1</div>
-                        <div className='level-bar-stat'><div className='level-bar-stat-w'></div></div>
-                    </div>
+                    <img src ={edit} width={100} className='Profile-img-edit'></img>
                 </div>
             </div>
-            // test code 
-            // <h1><a href="/" onClick={function (e) {
-            //     alert("안녕하세요");
-            //     e.preventDefault();
-            // }}>dtd</a>
-            // </h1>
-            // <h1 title="WEB">{this.props.title}</h1>
-            // world wide web!
             
-        );
-    }
+            <div className='info-box'>
+                <div className='Profile-name back-setting'>
+                    
+                    <div className='text-box'>Mule</div>
+                </div>
+                <div className='Profile-info back-setting'>
+                    <div className='text-box'>
+                        상메 글자수 제한해서 크기는 고정시키고 상메 글자수 제한해서 크기는 고정시키고
+                        상메 글자수 제한해서 크기는 고정시키고 상메 글자수 제한해서 크기는 고정시키고
+                    </div>
+                </div>
+                <div className='Profile-level'>
+                    <div className='level-stat'>Lv. 1</div>
+                    <div className='level-bar-stat'><div className='level-bar-stat-w'></div></div>
+                </div>
+            </div>
+        </div>
+    );    
 }
 
 export default Profile;
--- check-and-talk/src/components/ProfileEdit.js
+++ check-and-talk/src/components/ProfileEdit.js
@@ -1,27 +1,28 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './ProfileEdit.css'
 import Profile_red from '../assets/Profile_Red.svg';
 
-class ProfileEdit extends Component {
-    render() {
-        return (
-            <div className='ProfileEdit-style'>
-                
-                <div className='Profile-img'>
-                    <img src={Profile_red} width={269}></img>
+function ProfileEdit() {
+    var colorList = ["red", "yellow", "green", "blue", "purple"]
+    
+    return (
+        <div className='ProfileEdit-style'>
+            
+            <div className='Profile-img'>
+                <img src={Profile_red} width={269}></img>
+            </div>
+            <div className='ProfileEdit-color'>
+                <div className='color-info'>
+                    {colorList.map((v) => (<div className={v}></div>))}
                 </div>
-                <div className='ProfileEdit-color'>
-                    <div className='color-info'>
-                        {colorList.map((v) => (<div className={v}></div>))}
-                    </div>
-                    <div className='check-cancel'>
-                        <div className='check cell'>확인</div>
-                        <div className='cancel cell'>취소</div>
-                    </div>
+                <div className='check-cancel'>
+                    <div className='cancel cell'>취소</div>
+                    <div className='check cell'>확인</div>                        
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
+    
 }
 
 export default ProfileEdit;
