688f20b3dc1c2b3bc3474b32592b62dfb75d1dc3
HanGyeongjun <030212hgjun@gmail.com>
2023-05-31 15:32:04 +0900

    [Feat] #1 - Create web header(navigation bar) code

--- check-and-talk/src/App.js
+++ check-and-talk/src/App.js
@@ -3,22 +3,36 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div class="App">
+      <header class="navigationHeader">
+        <img src={logo} alt="Logo" class="catLogo" />
+        <nav class="navigationArea">
+          <a href="연결할링크#" class="navigationTalk">Check</a>
+          <a href="연결할링크#" class="navigationTalk">Talk</a>
+        </nav>
+        <a href="#" class="signOutButton">로그아웃</a>
       </header>
     </div>
+
+
+
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
